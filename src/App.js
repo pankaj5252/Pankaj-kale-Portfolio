fb91e5a7b540281311596ef881e236ed964e4afe
pankaj5252 <pbkale25@gmail.com>
2024-07-15 15:54:14 +0530

    Fix bugs

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import './components/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './components/PageNotFound';
-import Home from './components/Home';
+import Home from './components/Home'
 import Footer from './components/Footer';
 import LoadingScreen from './components/LoadingScreen';
 import { useState, useEffect } from 'react';
-import { Link as Element } from 'react-scroll';
 import boy from "./assets/boy1.png"
 
 function App() {
@@ -29,13 +28,7 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path="/" element={
-            <>
-              <Element name="home">
-                <Home data={boy1}/>
-              </Element>
-            </>
-          } />
+          <Route path="/" element={<Home data={boy1} />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
         <Footer />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,10 +5,8 @@ import { Slide, Zoom } from "react-awesome-reveal";
 import About from "./About";
 import Resume from "./Resume";
 import Services from "./Services";
-import { Link as ScrollLink } from "react-scroll";
 import Projects from "./Projects";
 import Contact from "./Contact";
-import ScrollToTop from "./ScrollToTop"; // Import the ScrollToTop component
 
 const Home = (props) => {
   return (
@@ -23,14 +21,12 @@ const Home = (props) => {
                 <Slide>
                   <h1 className="name">Hey! I'm Pankaj Kale</h1>
                   <h2>MERN Stack Developer</h2>
-                  <ScrollLink
-                    to="about"
-                    smooth={true}
-                    duration={400}
+                  <a
+                    href="#about"
                     className="relative btn-about py-2 px-8 text-base font-bold nded-full overflow-hidden bg-red text-white rounded-full transition-all duration-400 ease-in-out shadow-md hover:scale-105 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-blue-600 before:to-blue-300 before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0"
                   >
                     About Me
-                  </ScrollLink>
+                  </a>
                 </Slide>
               </div>
             </section>
@@ -65,8 +61,6 @@ const Home = (props) => {
       {/* Contact */}
       <Contact />
 
-      {/* Scroll to Top Button */}
-      <ScrollToTop />
     </>
   );
 };
